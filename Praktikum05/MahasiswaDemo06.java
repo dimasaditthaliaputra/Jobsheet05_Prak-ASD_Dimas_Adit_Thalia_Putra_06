6a7f043441415ddaeecbb4d5301bbe1f01849285
dimasaditt <aditcraftyt@gmail.com>
2025-03-19 13:08:49 +0700

    Commit Jobsheet 5 Latihan 🧭

--- Praktikum05/MahasiswaDemo06.java
+++ Praktikum05/MahasiswaDemo06.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MahasiswaMain06 {
+public class MahasiswaDemo06 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         MahasiswaBerprestasi06 list = new MahasiswaBerprestasi06();
